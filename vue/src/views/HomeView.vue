420db92c9627fcbdb6af021e742524602da90fb9
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2024-03-11 12:42:03 -0400

    commit idk

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -7,7 +7,7 @@ import { ref, Created } from 'vue'
 const squirrel = ref('')
 async function getSquirrelData() {
   let response = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
-  let data = await res.json()
+  let data = await response.json()
   squirrel.value = data.results
 
 Created(() => {
