0c967a68c793062e4db9df70eb51bab0206e33fe
ShaoShaoUwU <73066684+ShaoShaoUwU@users.noreply.github.com>
2024-03-19 13:12:30 -0400

    `12232

--- vue/src/components/BarChart.vue
+++ vue/src/components/BarChart.vue
@@ -2,6 +2,7 @@
   <Bar id="my-chart-id" :options="chartOptions" :data="chartData" v-if="loaded"/>
 </template>
 
+
 <script>
 import { Bar } from 'vue-chartjs'
 import {
@@ -14,26 +15,34 @@ import {
   LinearScale
 } from 'chart.js'
 
+
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
 
 export default {
   name: 'BarChart',
   components: { Bar },
   data: () => ({
     loaded: false,
-    chartData: null
-      }),
-     async mounted (){
-      this.loaded = false 
+    chartData: null,
+    chartOptions: {} 
+  }),
+  async mounted() {
+    this.loaded = false;
 
-      try {
-        const {color} = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
-        this.chartdata = color
 
-        this.loaded = true
-      } catch (e){
-        console.error(e)
+    try {
+      const response = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json');
+      if (response.ok) {
+        const data = await response.json();
+        this.chartData = data;
+        this.loaded = true;
+      } else {
+        console.error('Failed to fetch data:', response.status);
       }
-     }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 }
 </script>
