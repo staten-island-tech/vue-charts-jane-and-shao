c7cf621a86d9b5922e0467fcbee15579d0370638
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2024-03-27 13:14:20 -0400

    iu487t43

--- vue/src/components/DoughChart.vue
+++ vue/src/components/DoughChart.vue
@@ -2,31 +2,48 @@
   <Doughnut :data="chartData" :options="options" />
 </template>
 
-<script lang="ts">
+<script>
+import { ref } from 'vue'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Doughnut } from 'vue-chartjs'
-
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
 export default {
-    name: 'DoughChart',
-    components: {
-        Doughnut
-    },
-    data() {
-        return{
-        chartData: {
-            labels: colors,
-                datasets: [{data: colornumbers}]
-        },
+  name: 'DoughChart',
+  components: { Doughnut },
+  data() {
+    return {
+      chartData: {
+        labels: ages,
+        datasets: [{ data: ageNumbers }]
+      },
 
-        const options = {
-            responsive: true,
-            maintainAspectRatio: false
-        }
-
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
     }
+  }
 }
+const squirrel = ref('')
+async function getSquirrelData() {
+  let response = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+  let data = await response.json()
+  console.log(data)
+  return data
 }
+
+squirrel.value = await getSquirrelData()
+
+const ages = []
+const ageNumbers = []
+for (let i = 0; i < squirrel.value.length; i++) {
+  if (squirrel.value[i].hasOwnProperty('age') && !ages.includes(squirrel.value[i].age)) {
+    ages.push(squirrel.value[i].age)
+    ageNumbers.push(1)
+  } else if (squirrel.value[i].hasOwnProperty('age')) {
+    let index = ages.indexOf(squirrel.value[i].age)
+    ageNumbers[index]++
+  }
 </script>
---
+++ vue/src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <DoughChart />
+  <div></div>
+</template>
+
+<script setup>
+import DoughChart from '../components/DoughChart.vue'
+import { ref, onMounted } from 'vue'
+</script>
+
+<style scoped></style>
